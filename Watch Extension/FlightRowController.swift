c44a1e189af5bf4ae258d28d139aba790614b6e0
Terry Wang <terry.wang@accenture.com>
2015-10-31 20:32:47 -0400

    created FlightRowController

---
+++ Watch Extension/FlightRowController.swift
@@ -0,0 +1,20 @@
+//
+//  FlightRowController.swift
+//  AirAber
+//
+//  Created by Terry Wang on 10/31/15.
+//  Copyright © 2015 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+
+class FlightRowController: NSObject {
+
+    @IBOutlet var separator: WKInterfaceSeparator!
+    @IBOutlet var originLabel: WKInterfaceLabel!
+    @IBOutlet var destinationLabel: WKInterfaceLabel!
+    @IBOutlet var flightNumberLabel: WKInterfaceLabel!
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var planeImage: WKInterfaceImage!
+    
+}
