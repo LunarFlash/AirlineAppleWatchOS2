b855c86bf8bcd9101b014c19a44129f92335daca
Terry Wang <terry.wang@accenture.com>
2015-10-31 20:27:58 -0400

    created flights ic

---
+++ Watch Extension/ScheduleInterfaceController.swift
@@ -0,0 +1,41 @@
+//
+//  ScheduleInterfaceController.swift
+//  AirAber
+//
+//  Created by Terry Wang on 10/31/15.
+//  Copyright © 2015 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ScheduleInterfaceController: WKInterfaceController {
+
+    
+    @IBOutlet var flightsTable: WKInterfaceTable!
+    
+    
+    var flights = Flight.allFlights()  // array of Flight objects
+    
+    
+    
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+        flightsTable.setNumberOfRows(flights.count, withRowType: "FlightRow")
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
